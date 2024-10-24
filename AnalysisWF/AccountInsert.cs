3716ba15f8f587e07f689481ec1a258549772339
Vladimir Đorđević <vladimir.djordjevic@extreme.rs>
2024-10-24 21:33:58 +0200

    2

---
+++ AnalysisWF/AccountInsert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace AnalysisWF
+{
+    public class AccountInsert : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            
+        }
+    }
+}
