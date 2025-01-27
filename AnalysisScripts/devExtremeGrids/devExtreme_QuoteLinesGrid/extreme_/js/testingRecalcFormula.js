8ee0962235b6c46ba05618908ea14ed4d8c66ea9
Lazar Cirko <lazar.cirkovic@extreme.rs>
2025-01-27 17:41:17 +0100

    #

--- AnalysisScripts/devExtremeGrids/devExtreme_QuoteLinesGrid/extreme_/js/testingRecalcFormula.js
+++ AnalysisScripts/devExtremeGrids/devExtreme_QuoteLinesGrid/extreme_/js/testingRecalcFormula.js
@@ -32,29 +32,18 @@ const fullPd = pdPerUnit * 1; // Assuming quantity is always 1
 
 function calculateAmounts(supplierPricePerUnit, margin, discount, supplierDiscount, defaultTax) {
     const taxRate = defaultTax / 100;
-
     const supplierBaseAmount = supplierPricePerUnit * 1; // Assuming quantity is always 1
-
     const pricePerUnit = Math.ceil(margin * supplierPricePerUnit);
-
     const baseAmount = pricePerUnit * 1; // Assuming quantity is always 1
-
     const fullPriceWithDiscount = pricePerUnit * (1 - discount / 100) * 1; // Assuming quantity is always 1
-
     const manualDiscountAmount = baseAmount - fullPriceWithDiscount;
-
     const tax = fullPriceWithDiscount * (1 + taxRate) - fullPriceWithDiscount;
-
     const extendedAmount = fullPriceWithDiscount + tax;
-
     const supplierDiscountAmount = supplierPricePerUnit * (supplierDiscount / 100);
     const pricePerUnitWithSupplierDiscount = supplierPricePerUnit - supplierDiscountAmount;
-
     const customDiscountAmount = pricePerUnit * (discount / 100);
     const pricePerUnitWithCustomDiscount = pricePerUnit - customDiscountAmount;
-
     const pdPerUnit = pricePerUnitWithCustomDiscount - pricePerUnitWithSupplierDiscount;
-
     const fullPd = pdPerUnit * 1; // Assuming quantity is always 1
 
     return {
