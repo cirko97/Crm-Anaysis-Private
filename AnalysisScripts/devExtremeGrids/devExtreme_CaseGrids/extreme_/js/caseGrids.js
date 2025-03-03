c4805e7eecae49fdf319bdf28f7a82c48a9791a6
Lazar Cirko <lazar.cirkovic@extreme.rs>
2025-03-03 11:27:43 +0100

    Add case asset retrieval and refresh logic in caseGrids.js

--- AnalysisScripts/devExtremeGrids/devExtreme_CaseGrids/extreme_/js/caseGrids.js
+++ AnalysisScripts/devExtremeGrids/devExtreme_CaseGrids/extreme_/js/caseGrids.js
@@ -148,7 +148,7 @@ async function setClientApiContext(Xrm, formContext) {
     );
 
     await Xrm.WebApi.retrieveMultipleRecords("extreme_caseline",
-      `?$select=extreme_caselineid,_extreme_asset_value,_extreme_case_value,extreme_name,_ownerid_value,_extreme_product_value,extreme_quantity,_extreme_unit_value
+      `?$select=extreme_caselineid,_extreme_caseasset_value,_extreme_asset_value,_extreme_case_value,extreme_name,_ownerid_value,_extreme_product_value,extreme_quantity,_extreme_unit_value
       &$expand=extreme_Product($select=producttypecode,name,productnumber)&$filter=_extreme_case_value eq ${caseId}`).then(
         function success(results) {
           console.log(results);
@@ -176,6 +176,9 @@ async function setClientApiContext(Xrm, formContext) {
             var extreme_unit = result["_extreme_unit_value"]; // Lookup
             var extreme_unit_formatted = result["_extreme_unit_value@OData.Community.Display.V1.FormattedValue"];
             var extreme_unit_lookuplogicalname = result["_extreme_unit_value@Microsoft.Dynamics.CRM.lookuplogicalname"];
+            var extreme_caseasset = result["_extreme_caseasset_value"]; // Lookup
+            var extreme_caseasset_formatted = result["_extreme_caseasset_value@OData.Community.Display.V1.FormattedValue"];
+            var extreme_caseasset_lookuplogicalname = result["_extreme_caseasset_value@Microsoft.Dynamics.CRM.lookuplogicalname"];
 
             // Many To One Relationships
             if (result.hasOwnProperty("extreme_Product") && result["extreme_Product"] !== null) {
@@ -196,7 +199,8 @@ async function setClientApiContext(Xrm, formContext) {
               'extreme_quantity': extreme_quantity,
               'extreme_producttypecode': extreme_Product_producttypecode,
               'productname': extreme_Product_productnumber ? extreme_Product_productnumber + ' - ' + extreme_Product_name : extreme_Product_name,
-              'extreme_unit': extreme_unit
+              'extreme_unit': extreme_unit,
+              'extreme_caseasset': extreme_caseasset
             });
 
           }
@@ -832,8 +836,6 @@ async function setClientApiContext(Xrm, formContext) {
 
             await Promise.all(promises);
 
-            // Refresh grid for case assets
-            await formContext.getControl('WebResource_caseAssets').getObject().contentWindow.window.setClientApiContext(Xrm, formContext);
           }
 
           // create N:N relationship
@@ -858,6 +860,9 @@ async function setClientApiContext(Xrm, formContext) {
                     console.log(error.message);
                   }
                 );
+
+                // Refresh grid for case assets
+                await formContext.getControl('WebResource_caseAssets').getObject().contentWindow.window.setClientApiContext(Xrm, formContext);
               }
               else {
                 var recordNN = {};
@@ -873,6 +878,10 @@ async function setClientApiContext(Xrm, formContext) {
                     console.log(error.message);
                   }
                 );
+
+                // Refresh grid for case assets
+                await formContext.getControl('WebResource_caseAssets').getObject().contentWindow.window.setClientApiContext(Xrm, formContext);
+
               }
             },
             function (error) {
