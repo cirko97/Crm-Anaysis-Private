e20e5ac6a5a145da9afeaa682131a5b865f99348
Lazar Cirko <lazar.cirkovic@extreme.rs>
2025-03-06 10:12:57 +0100

    Add loading overlay and update inventory table structure for improved user experience

--- AnalysisScripts/devExtremeGrids/devExtreme_QuoteLinesGrid/extreme_/js/inventoryInfoList.js
+++ AnalysisScripts/devExtremeGrids/devExtreme_QuoteLinesGrid/extreme_/js/inventoryInfoList.js
@@ -1,6 +1,4 @@
-$(() => {
-
-    let pawsConfig = {};
+$(async () => {
 
     var executeWorkflowRequest = {
         entity: { entityType: "workflow", id: "1738674e-adf9-ef11-bae2-6045bd9d91cf" },
@@ -20,24 +18,56 @@ $(() => {
     };
 
     // Execute workflow
-    Xrm.WebApi.execute(executeWorkflowRequest).then(
+    await parent.Xrm.WebApi.execute(executeWorkflowRequest).then(
         function success(response) {
             if (response.ok) {
                 console.log("Workflow executed successfully.");
 
                 // Now retrieve the API response from the quotedetail record
                 var recordId = queryParams.quoteDetailGuid;  // Replace with dynamic ID
-                return Xrm.WebApi.retrieveRecord("quotedetail", recordId, "?$select=extreme_apiresponse");
+                return parent.Xrm.WebApi.retrieveRecord("quotedetail", recordId, "?$select=extreme_apiresponse");
             }
         }
     ).then(function (retrievedRecord) {
         if (retrievedRecord && retrievedRecord.extreme_apiresponse) {
             console.log("API Response:", retrievedRecord.extreme_apiresponse);
+
+            const apiResponse = JSON.parse(retrievedRecord.extreme_apiresponse);
+            console.log(apiResponse[0]["tHE_Stock"]);
+
+            if (apiResponse[0]["tHE_Stock"].length === 0 || apiResponse[0]["tHE_Stock"] === null || apiResponse[0]["tHE_Stock"] === undefined) {
+                console.log("No stock found for this item.");
+            }
+            else {
+                console.log("Stock found for this item.");
+
+                for (let i = 0; i < apiResponse[0]["tHE_Stock"].length; i++) {
+                    const stockItem = apiResponse[0]["tHE_Stock"][i];
+                    console.log(stockItem);
+
+                    // Add stock item to the table
+                    $("#inventoryInfoList").append(`
+                        <tr>
+                            <td>${stockItem["acWarehouse"]}</td>
+                            <td>${stockItem["anStock"]}</td>
+                            <td>${stockItem["anReserved"]}</td>
+                        </tr>
+                    `);
+
+                }
+
+            }
         } else {
             console.log("No API response found on record.");
         }
     }).catch(function (error) {
         console.log("Error:", error.message);
+    });
+
+    $("#loadingOverlay").hide();
+
+    $("#ok-btn").on("click", function () {
+        window.close();
     });
 
 });
