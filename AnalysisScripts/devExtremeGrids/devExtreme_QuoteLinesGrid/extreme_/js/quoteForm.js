c53f4c583d2313f082e01fa9c18cc9fddda45617
Lazar Cirko <lazar.cirkovic@extreme.rs>
2025-10-24 11:55:34 +0200

    Enhance currency handling in quote form by updating transaction currency based on customer selection and ensuring proper validation for quote products.

--- AnalysisScripts/devExtremeGrids/devExtreme_QuoteLinesGrid/extreme_/js/quoteForm.js
+++ AnalysisScripts/devExtremeGrids/devExtreme_QuoteLinesGrid/extreme_/js/quoteForm.js
@@ -210,66 +210,76 @@ async function form_onload(executionContext) {
             if (customerValue !== null && customerValue.length > 0) {
                 var customerId = customerValue[0].id;
                 const accTransaction = await Xrm.WebApi.retrieveRecord("account", customerId, "?$select=_transactioncurrencyid_value");
-                const initCustomerId = initCustomer && initCustomer.length > 0 ? initCustomer[0].id : null;
-                const initCustomerTransactionId = initCustomerId ? (await Xrm.WebApi.retrieveRecord("account", initCustomerId, "?$select=_transactioncurrencyid_value"))["_transactioncurrencyid_value"] : null;
-                // If changing customer, and initial customer had different currency, and there are quote products, prevent change
-                if (initCustomerId && customerId !== initCustomerId && initCustomerTransactionId && accTransaction && accTransaction._transactioncurrencyid_value && initCustomerTransactionId.toLowerCase() !== accTransaction._transactioncurrencyid_value.toLowerCase()) {
-                    if (accTransaction && accTransaction._transactioncurrencyid_value) {
-                        var currentCurrency = formContext.getAttribute("transactioncurrencyid").getValue();
-                        var accountCurrencyId = accTransaction["_transactioncurrencyid_value"];
-                        if (
-                            !currentCurrency ||
-                            currentCurrency[0].id.toLowerCase() !== accountCurrencyId.toLowerCase()
-                        ) {
-                            if (formType !== FORM_NEW) {
-                                // Check if there are any quotedetails (quote products) for this quote
-                                const quoteId = formContext.data.entity.getId().replace(/[{}]/g, "");
-                                const quotedetailsResult = await Xrm.WebApi.retrieveMultipleRecords(
-                                    "quotedetail",
-                                    `?$select=quotedetailid&$filter=_quoteid_value eq ${quoteId}&$top=1`
-                                );
+                
+                if (accTransaction && accTransaction._transactioncurrencyid_value) {
+                    var currentQuoteCurrency = formContext.getAttribute("transactioncurrencyid").getValue();
+                    var newCustomerCurrencyId = accTransaction["_transactioncurrencyid_value"];
+                    
+                    // Check if quote currency is different from new customer currency
+                    // Remove curly braces for comparison
+                    var currentQuoteCurrencyId = currentQuoteCurrency ? currentQuoteCurrency[0].id.replace(/[{}]/g, "").toLowerCase() : null;
+                    var newCustomerCurrencyIdClean = newCustomerCurrencyId.replace(/[{}]/g, "").toLowerCase();
+                    
+                    if (currentQuoteCurrencyId && currentQuoteCurrencyId !== newCustomerCurrencyIdClean) {
+                        if (formType !== FORM_NEW) {
+                            // Check if there are any quotedetails (quote products) for this quote
+                            const quoteId = formContext.data.entity.getId().replace(/[{}]/g, "");
+                            const quotedetailsResult = await Xrm.WebApi.retrieveMultipleRecords(
+                                "quotedetail",
+                                `?$select=quotedetailid&$filter=_quoteid_value eq ${quoteId}&$top=1`
+                            );
 
-                                if (quotedetailsResult.entities && quotedetailsResult.entities.length > 0) {
-                                    Xrm.Utility.alertDialog("Before changing the customer or currency, you must delete all Quote products.");
-                                    // Revert customer back to initial value
-                                    formContext.getAttribute("customerid").setValue(initCustomer);
-                                    await formContext.data.refresh(false);
-                                    return;
-                                }
-                            }
-                            var transactionCurrencyLookup = [{
-                                id: accountCurrencyId,
-                                name: accTransaction["_transactioncurrencyid_value@OData.Community.Display.V1.FormattedValue"],
-                                entityType: "transactioncurrency"
-                            }];
-                            formContext.getAttribute("transactioncurrencyid").setValue(transactionCurrencyLookup);
-                            if (formType !== FORM_NEW) {
-                                const quoteIdForm = formContext.data.entity.getId().replace(/[{}]/g, "");
-                                const fieldsToNull = [
-                                    "extreme_chfexchangerate",
-                                    "extreme_dollarexchangerate",
-                                    "extreme_euroexchangerate",
-                                    "exchangerate",
-                                    "extreme_gbpexchangerate",
-                                    "extreme_macedoniandenarexchangerate",
-                                    "extreme_rsdexchangerate"
-                                ];
-                                const recordToUpdate = {};
-                                fieldsToNull.forEach(field => recordToUpdate[field] = null);
-
-                                await Xrm.WebApi.updateRecord("quote", quoteIdForm, recordToUpdate);
-                            }
-                            retryAttempt(() => setClientApiContextForWebResource(formContext, "WebResource_quoteLines"));
-                            if (formType !== FORM_NEW) {
-                                await formContext.data.refresh(true);
+                            if (quotedetailsResult.entities && quotedetailsResult.entities.length > 0) {
+                                Xrm.Utility.alertDialog("Before changing the customer or currency, you must delete all Quote products.");
+                                // Revert customer back to initial value
+                                formContext.getAttribute("customerid").setValue(initCustomer);
+                                await formContext.data.refresh(false);
+                                return;
                             }
                         }
-                    } else {
-                        formContext.getAttribute("transactioncurrencyid").setValue(null);
+                        
+                        // No products or new form - update currency to match new customer
+                        var transactionCurrencyLookup = [{
+                            id: newCustomerCurrencyId,
+                            name: accTransaction["_transactioncurrencyid_value@OData.Community.Display.V1.FormattedValue"],
+                            entityType: "transactioncurrency"
+                        }];
+                        formContext.getAttribute("transactioncurrencyid").setValue(transactionCurrencyLookup);
+                        
+                        if (formType !== FORM_NEW) {
+                            const quoteIdForm = formContext.data.entity.getId().replace(/[{}]/g, "");
+                            const fieldsToNull = [
+                                "extreme_chfexchangerate",
+                                "extreme_dollarexchangerate",
+                                "extreme_euroexchangerate",
+                                "exchangerate",
+                                "extreme_gbpexchangerate",
+                                "extreme_macedoniandenarexchangerate",
+                                "extreme_rsdexchangerate"
+                            ];
+                            const recordToUpdate = {};
+                            fieldsToNull.forEach(field => recordToUpdate[field] = null);
+
+                            await Xrm.WebApi.updateRecord("quote", quoteIdForm, recordToUpdate);
+                        }
+                        retryAttempt(() => setClientApiContextForWebResource(formContext, "WebResource_quoteLines"));
+                        if (formType !== FORM_NEW) {
+                            await formContext.data.refresh(true);
+                        }
+                    } else if (!currentQuoteCurrency) {
+                        // If quote has no currency set, set it to customer's currency
+                        var transactionCurrencyLookup = [{
+                            id: newCustomerCurrencyId,
+                            name: accTransaction["_transactioncurrencyid_value@OData.Community.Display.V1.FormattedValue"],
+                            entityType: "transactioncurrency"
+                        }];
+                        formContext.getAttribute("transactioncurrencyid").setValue(transactionCurrencyLookup);
+                        if (formType !== FORM_NEW) {
+                            await formContext.data.refresh(true);
+                        }
                     }
-                }
-                else {
-                    await formContext.data.refresh(true);
+                } else {
+                    formContext.getAttribute("transactioncurrencyid").setValue(null);
                 }
             } else {
                 formContext.getAttribute("transactioncurrencyid").setValue(null);
